3be718e975fc6cf9eeed5be3e34ce4791200ff44
Nadja Yanna Braga <nadjayannabr@gmail.com>
2017-06-13 13:26:44 +0200

    app multiplica a idade digitada por 7

--- DogsAge/app/src/main/java/dogsage/cursoandroid/com/dogsage/MainActivity.java
+++ DogsAge/app/src/main/java/dogsage/cursoandroid/com/dogsage/MainActivity.java
@@ -2,12 +2,41 @@ package dogsage.cursoandroid.com.dogsage;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText idade;
+    private Button btnCalcular;
+    private TextView resultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        idade = (EditText) findViewById(R.id.idadeId);
+        btnCalcular = (Button) findViewById(R.id.btnCalcularId);
+        resultado = (TextView) findViewById(R.id.resultadoIdadeId);
+
+        btnCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(idade.getText().toString().isEmpty()){
+                    //mensagem de erro
+                    resultado.setText("Nenhum número digitado");
+                }else{
+
+                    int valorDigitado = Integer.parseInt(idade.getText().toString());
+
+                    resultado.setText("The age of the dog in human years is " + valorDigitado*7 +"!");
+                }
+            }
+        });
+
     }
 }
