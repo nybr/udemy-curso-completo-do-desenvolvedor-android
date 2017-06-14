ea0e17da6e7c4b331203f3e19fc1bb83a43c5d93
Nadja Yanna Braga <nadjayannabr@gmail.com>
2017-06-14 18:06:14 +0200

    Servicos funcionando

--- AtmEmpresaConsultoria/app/src/main/java/atmempresaconsultoria/cursoandroid/com/atmempresaconsultoria/MainActivity.java
+++ AtmEmpresaConsultoria/app/src/main/java/atmempresaconsultoria/cursoandroid/com/atmempresaconsultoria/MainActivity.java
@@ -10,6 +10,9 @@ import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity {
 
     private ImageView empresa;
+    private ImageView servicos;
+   // private ImageView clientes;
+    //private ImageView contatos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +20,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         empresa = (ImageView) findViewById(R.id.empresaId);
+        servicos = (ImageView) findViewById(R.id.servicosId);
+     //   clientes = (ImageView) findViewById(R.id.clientesId);
+     //   contatos = (ImageView) findViewById(R.id.contatosId);
 
         empresa.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, EmpresaActivity.class));
+            }
+        });
+
+        servicos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ServicosActivity.class));
             }
         });
     }
