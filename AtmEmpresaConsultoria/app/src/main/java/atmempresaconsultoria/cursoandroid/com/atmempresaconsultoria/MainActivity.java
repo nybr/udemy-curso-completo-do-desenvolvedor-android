4a100f80f5a1faa62e6bda2af20fc1e034477f4a
Nadja Yanna Braga <nadjayannabr@gmail.com>
2017-06-14 17:50:51 +0200

    detalhes funcionando

--- AtmEmpresaConsultoria/app/src/main/java/atmempresaconsultoria/cursoandroid/com/atmempresaconsultoria/MainActivity.java
+++ AtmEmpresaConsultoria/app/src/main/java/atmempresaconsultoria/cursoandroid/com/atmempresaconsultoria/MainActivity.java
@@ -1,13 +1,28 @@
 package atmempresaconsultoria.cursoandroid.com.atmempresaconsultoria;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView empresa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        empresa = (ImageView) findViewById(R.id.empresaId);
+
+        empresa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, EmpresaActivity.class));
+            }
+        });
     }
 }
