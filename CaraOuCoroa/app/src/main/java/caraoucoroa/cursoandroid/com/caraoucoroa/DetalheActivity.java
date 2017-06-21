8a32169ac213a031baee54b96ac218c831bf137a
Nadja Yanna Braga <nadjayannabr@gmail.com>
2017-06-21 13:08:01 +0200

    Interface pronta

---
+++ CaraOuCoroa/app/src/main/java/caraoucoroa/cursoandroid/com/caraoucoroa/DetalheActivity.java
@@ -0,0 +1,13 @@
+package caraoucoroa.cursoandroid.com.caraoucoroa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetalheActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhe);
+    }
+}
