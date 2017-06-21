66bd7333e78b6f1cf7123c146d4956b239fa0a53
Nadja Yanna Braga <nadjayannabr@gmail.com>
2017-06-21 16:24:54 +0200

    Funcionando cara e coroa

--- CaraOuCoroa/app/src/main/java/caraoucoroa/cursoandroid/com/caraoucoroa/DetalheActivity.java
+++ CaraOuCoroa/app/src/main/java/caraoucoroa/cursoandroid/com/caraoucoroa/DetalheActivity.java
@@ -1,13 +1,45 @@
 package caraoucoroa.cursoandroid.com.caraoucoroa;
 
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class DetalheActivity extends AppCompatActivity {
+
+    private ImageView imagem;
+    private ImageView btnVoltar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalhe);
+
+        imagem = (ImageView) findViewById(R.id.imagemMoedaId);
+        btnVoltar = (ImageView) findViewById(R.id.botaoVoltarId);
+
+        Bundle extra = getIntent().getExtras();
+
+        if(extra != null){
+
+            int valor = extra.getInt("opcao");
+
+            if (valor == 0){
+                imagem.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.moeda_cara));
+            }else if (valor == 1){
+                imagem.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.moeda_coroa));
+            }else {
+                imagem.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.logo));
+            }
+
+        }
+
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
--- CaraOuCoroa/app/src/main/java/caraoucoroa/cursoandroid/com/caraoucoroa/MainActivity.java
+++ CaraOuCoroa/app/src/main/java/caraoucoroa/cursoandroid/com/caraoucoroa/MainActivity.java
@@ -1,13 +1,39 @@
 package caraoucoroa.cursoandroid.com.caraoucoroa;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView botaoJogar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botaoJogar = (ImageView) findViewById(R.id.btnJogarId);
+
+        botaoJogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Random randomico = new Random();
+                int num = randomico.nextInt(2);
+
+                // 0 cara - 1 coroa
+
+                Intent intent = new Intent(MainActivity.this, DetalheActivity.class);
+                intent.putExtra("opcao", num);
+                startActivity(intent);
+
+            }
+        });
     }
 }
